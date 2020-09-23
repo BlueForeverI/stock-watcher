10b9c17e801a69cac17035663ce5aee42fb65c16
Georgi Yolovski <blueforeverabv@gmail.com>
2020-09-23 22:24:36 +0300

    Added content-type response header

--- database.go
+++ database.go
@@ -37,8 +37,16 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(product)
 }
 
+func commonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/products", allProducts).Methods("GET")
 	myRouter.HandleFunc("/products/{id}", productByID)
 	myRouter.HandleFunc("/products", createProduct).Methods("POST")
