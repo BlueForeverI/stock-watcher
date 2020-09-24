8578cd6ab7863618d73459b60e5bd6179ebe7a16
georgi.yolovski <georgi.yolovski@gannett.com>
2020-09-24 19:15:22 +0300

    Working on the user-stock association

--- user.go
+++ user.go
@@ -7,5 +7,5 @@ import (
 type User struct {
 	gorm.Model
 	Email  string
-	Stocks []Stock `gorm:"many2many:user_stocks;"`
+	Stocks []Stock `gorm:"many2many:user_stocks;association_autoupdate:false;association_autocreate:false"`
 }
