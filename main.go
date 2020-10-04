ece9c7813e25295820890a2e740b91e4a7d76f89
Georgi Yolovski <blueforeverabv@gmail.com>
2020-10-04 13:18:17 +0300

    Updated allowed methods

--- main.go
+++ main.go
@@ -32,6 +32,8 @@ func commonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type")
 		next.ServeHTTP(w, r)
 	})
 }
